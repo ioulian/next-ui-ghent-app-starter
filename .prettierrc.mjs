389817450b0f6144144a63ea648608c6b0f0723f
Yulian Alexeyev <yulian.alexeyev+github@gmail.com>
2024-02-14 12:59:51 +0100

    add prettier and husky

---
+++ .prettierrc.mjs
@@ -0,0 +1,11 @@
+// prettier.config.js, .prettierrc.js, prettier.config.mjs, or .prettierrc.mjs
+
+/** @type {import("prettier").Config} */
+const config = {
+  trailingComma: "all",
+  tabWidth: 2,
+  semi: true,
+  singleQuote: false,
+};
+
+export default config;
