f100519d18714681670a7b8d6780a8e79aebbb41
Yulian Alexeyev <yulian.alexeyev+github@gmail.com>
2024-02-27 15:53:21 +0100

    react pagination a11y

--- src/components/common/pagination/Pagination.tsx
+++ src/components/common/pagination/Pagination.tsx
@@ -1,7 +1,8 @@
 "use client";
 
-import { FC, memo } from "react";
+import { Component, FC, memo, useEffect, useRef } from "react";
 import ReactPaginate, { ReactPaginateProps } from "react-paginate";
+import { useTranslations } from "next-intl";
 
 import { cx } from "@/styled-system/css";
 
@@ -14,20 +15,39 @@ import {
 } from "./Pagination.styles";
 
 const Pagination: FC<ReactPaginateProps> = (props) => {
+  const t = useTranslations("common.pagination");
+  const ref = useRef<Component<ReactPaginateProps> | null>(null);
+
+  useEffect(() => {
+    if (ref.current) {
+      console.log(ref.current);
+    }
+  }, [ref]);
+
   return (
-    <ReactPaginate
-      {...{
-        ...props,
-        className: cx(pagination, props.className),
-        pageClassName: cx(paginationPage, props.pageClassName),
-        pageLinkClassName: cx(paginationPageLink, props.pageLinkClassName),
-        disabledClassName: cx(paginationDisabled, props.disabledClassName),
-        nextLinkClassName: cx(paginationPageLink, props.nextLinkClassName),
-        previousLinkClassName: cx(paginationPageLink, props.previousLinkClassName),
-        breakLinkClassName: cx(paginationPageLink, props.breakLinkClassName),
-        activeClassName: cx(paginationPageSelected, props.activeClassName),
-      }}
-    />
+    <div role="navigation">
+      <ReactPaginate
+        ref={ref}
+        {...{
+          ...props,
+          breakAriaLabels: {
+            backward: t("aria.break.backward"),
+            forward: t("aria.break.forward"),
+          },
+          ariaLabelBuilder: (page) => t("aria.page", { page }),
+          previousAriaLabel: t("aria.previous"),
+          nextAriaLabel: t("aria.next"),
+          className: cx(pagination, props.className),
+          pageClassName: cx(paginationPage, props.pageClassName),
+          pageLinkClassName: cx(paginationPageLink, props.pageLinkClassName),
+          disabledClassName: cx(paginationDisabled, props.disabledClassName),
+          nextLinkClassName: cx(paginationPageLink, props.nextLinkClassName),
+          previousLinkClassName: cx(paginationPageLink, props.previousLinkClassName),
+          breakLinkClassName: cx(paginationPageLink, props.breakLinkClassName),
+          activeClassName: cx(paginationPageSelected, props.activeClassName),
+        }}
+      />
+    </div>
   );
 };
 
